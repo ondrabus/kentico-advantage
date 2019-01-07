1871a24d743abd43f5bfdfcad464e560c9303a6e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-07 15:10:20 +0000

    Deploy Kentico/kentico-advantage to github.com/Kentico/kentico-advantage.git:master

--- component---src-pages-about-js-b118890b055ea98c70c6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{137:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(142);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}},142:function(e,t,n){"use strict";var a=n(0),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
-//# sourceMappingURL=component---src-pages-about-js-b118890b055ea98c70c6.js.map
